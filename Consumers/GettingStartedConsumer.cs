a9f8d58d34193687886b8cec730607af72494b69
thecsharper <si@thecsharper.com>
2022-09-06 12:28:30 +0100

    Initial Commit

---
+++ Consumers/GettingStartedConsumer.cs
@@ -0,0 +1,23 @@
+namespace GettingStarted.Consumers;
+
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+using Contracts;
+using MassTransit;
+
+public class GettingStartedConsumer : IConsumer<GettingStarted>
+{
+    private readonly ILogger<GettingStartedConsumer> _logger;
+
+    public GettingStartedConsumer(ILogger<GettingStartedConsumer> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Consume(ConsumeContext<GettingStarted> context)
+    {
+        _logger.LogInformation("Received Text: {Text}", context.Message.Value);
+        return Task.CompletedTask;
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,30 @@
+﻿namespace GettingStarted;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+
+using Contracts;
+using MassTransit;
+
+public class Worker : BackgroundService
+{
+    readonly IBus _bus;
+
+    public Worker(IBus bus)
+    {
+        _bus = bus;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await _bus.Publish(new GettingStarted { Value = $"The time is {DateTimeOffset.Now}" }, stoppingToken);
+
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
